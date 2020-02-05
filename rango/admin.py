c11e70040d4be5e5c4cc68b8375712b91a0fca81
Krishang Patney <2405690p@student.gla.ac.uk>
2020-02-05 01:39:20 +0000

    Might revert to this later on, Chapter 6, populate not working...

--- rango/admin.py
+++ rango/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from rango.models import Category, Page
-
+    
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
 
-admin.site.register(Category)
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin) 
